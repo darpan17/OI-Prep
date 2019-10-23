90aef91c4ff2da71f35b979adc1a34ada38adbb6
darpan17 <54431407+darpan17@users.noreply.github.com>
2019-10-23 21:59:37 +0530

    Created on: 23 October 2019

---
+++ AtCoderDPContest/G_LongestPath.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,n) for(int i = 0; i < n; i++)
+#define FORE(i,a,b) for(int i = a; i <= b; i++)
+#define vi vector<int>
+#define pb push_back
+
+using namespace std;
+
+const int MAXN = 100005;
+int dp[MAXN];
+vi g[MAXN],par[MAXN];
+bool vis[MAXN];
+
+void dfs(int node){
+	if(par[node].empty()) dp[node] = 0;
+	for(auto i:par[node]){
+		if(dp[i] != INT_MIN) dp[node] = max(dp[node],dp[i]+1);
+		else{
+			dfs(i);
+			dp[node] = max(dp[node],dp[i]+1);
+		}
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(false); cin.tie(0);
+	int n,m; cin >> n >> m;
+	int x,y;
+	while(m--){
+		cin >> x >> y;
+		x--; y--;
+		g[x].pb(y);
+		par[y].pb(x);
+	}
+	FOR(i,n) dp[i] = INT_MIN;
+	FOR(i,n){
+		dfs(i);
+	}
+	int ans = 0;
+	FOR(i,n) ans = max(ans,dp[i]);
+	cout << ans;
+	return 0;
+}
