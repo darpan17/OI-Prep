d5eb1a825eb662a922ed96703a42516e917aff20
darpan17 <54431407+darpan17@users.noreply.github.com>
2019-11-17 14:50:31 +0530

    AC

---
+++ AtCoderDPContest/E_Knapsack2.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,n) for(int i = 0; i < n; i++)
+#define FORE(i,a,b) for(int i = a; i <= b; i++)
+#define ll long long
+
+using namespace std;
+
+ll dp[105][100005],wt[105];
+int val[105];
+
+int main(){
+	int n; ll w; cin >> n >> w;
+	FORE(i,1,n) cin >> wt[i] >> val[i];
+	int pre[n+5] = {0}; FORE(i,1,n) pre[i] = pre[i-1]+val[i];
+	FORE(i,1,100005) dp[0][i] = 123456789123456789;
+	FORE(i,1,n+1){
+		FOR(j,100005){
+			if(val[i] > j){
+				dp[i][j] = dp[i-1][j];	
+			}else{
+				dp[i][j] = min(dp[i-1][j],dp[i-1][j-val[i]]+wt[i]);
+			}
+		}
+	}
+	int ans = 0;
+	for(int i = 100004; i > 0; i--){
+		if(dp[n][i] <= w){
+			ans = i;
+			break;
+		}
+	}
+	cout << ans;
+	return 0;
+}
