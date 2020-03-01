9c7eecf280c9e040e6cec3c7841ad84d7696ee98
darpan17 <54431407+darpan17@users.noreply.github.com>
2020-03-01 11:43:55 +0530

    AC

--- USACO/2019-2020/FEB/Bronze/traingles_p1.cpp
+++ USACO/2019-2020/FEB/Bronze/traingles_p1.cpp
@@ -1,127 +1,28 @@
-/**
- * @author Darpan Bhattacharya
- * Created on: 23.02.2020 00:00:54
- * Description: USACO FEB BRONZE
- */ 
-
-/*Template begins*/
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long 
 
-typedef long long ll;
-typedef long double ld;
-
-typedef pair<int,int> pi;
-typedef pair<ll,ll> pl;
-typedef pair<ld,ld> pd;
-#define mp make_pair
-#define F first
-#define S second
-
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<ld> vd;
-typedef vector<string> vs;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
-#define pb push_back
-
-const ll MOD = 1e9+7;
-const int INF = 1e9+9;
-const ll LINF = 1e18+18;
-const int MAXN = 2e5+5;
-
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-template <class T> 
-using o_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
-
-#define FOR(i,a,b) for(auto (i) = (a); (i) <= (b); (i)++)
-#define F0R(i,a) FOR(i,0,(a)-1)
-#define ROF(i,a,b) for(auto (i) = (a); (i) >= (b); (i)--)
-#define R0F(i,a) ROF(i,(a),0)
-#define trav(i,a) for(auto& i:a)
-
-#define ins insert
-#define SZ(x) ((int)(x).size())
-#define all(x) x.begin(),x.end()
-#define rall(x) x.rbegin(),x.rend()
-
-ll fxp(ll x,ll y,ll p = MOD) {
-    if(y == 0) return 1; if(y == 1) return (x % p);
-    ll res = 1; res *= fxp(x,y/2,p); res %= p; res *= res; res %= p; if(y&1) res *= x; res %= p;
-    return res;
-}
-
-template<class P,class Q>
-ostream& operator << (ostream& out, pair<P,Q> p) {
-    out << "(" << p.first << ", " << p.second << ")";
-}
-template<class T>
-ostream& operator << (ostream& out, vector<T> x) { 
-    out << "{"; bool f = true; 
-    for(auto i:x) { if(f) out << i; else out << ", " << i; f = false; }
-    out << "}\n";
-}
-
-#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-void fin(string s) { freopen(s.c_str(),"r",stdin); }
-void fout(string s) { freopen(s.c_str(),"w",stdout); }
-#ifdef LOCAL
-#define fileio fin("inp.txt");
-#else
-#define fileio do{} while(false); 
-#define cerr if(false) cerr
-#endif
-/*Template ends*/ 
-
-void solve() {
+int32_t main() {
+    //~ freopen("inp.txt","r",stdin);
+    freopen("triangles.in","r",stdin);
+    freopen("triangles.out","w",stdout);
+    ios_base::sync_with_stdio(false); cin.tie(0);
     int n; cin >> n;
-    vpl pts(n); F0R(i,n) cin >> pts[i].F >> pts[i].S; 
-    ll ans = 0;
-    F0R(i,n) {
-        FOR(j,i+1,n-1) {
-            FOR(k,j+1,n-1) {
-                vpl temp = {pts[i],pts[j],pts[k]};
-                sort(all(temp));
-                //~ cout << temp;
-                // 1 centre
-                if((abs(temp[0].F) == abs(temp[1].F) && abs(temp[1].S) == abs(temp[2].S)) || (abs(temp[0].S) == abs(temp[1].S) && abs(temp[1].F) == abs(temp[2].F))) {
-                    if((abs(temp[0].F) == abs(temp[1].F) && abs(temp[1].S) == abs(temp[2].S))) {
-                        ans = max(ans,abs(temp[0].S-temp[1].S)*abs(temp[1].F-temp[2].F));
-                    } else {
-                        ans = max(ans,abs(temp[0].F-temp[1].F)*abs(temp[1].S-temp[2].S));
-                    }
+    pair<int,int> a[n];
+    for(int i = 0; i < n; i++) cin >> a[i].first >> a[i].second;
+    int ans = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            for(int k = 0; k < n; k++) {
+                if(a[j].first == a[i].first && a[j].second == a[k].second) {
+                    int temp = abs(a[j].second-a[i].second)*abs(a[j].first-a[k].first);
+                    if(temp < 0) temp *= -1;
+                    //~ cout << "temp = " << temp << endl;
+                    ans = max(ans,temp);
                 }
-               /* // 0 centre
-                if((abs(temp[0].F) == abs(temp[1].F) && abs(temp[0].S) == abs(temp[2].S)) || (abs(temp[0].S) == abs(temp[1].S) && abs(temp[0].F) == abs(temp[2].F))) {
-                    if((abs(temp[0].F) == abs(temp[1].F) && abs(temp[0].S) == abs(temp[2].S))) {
-                        ans = max(ans,abs(temp[0].S-temp[1].S)*abs(temp[0].F-temp[2].F));
-                    } else {
-                        ans = max(ans,abs(temp[0].F-temp[1].F)*abs(temp[0].S-temp[2].S));
-                    }
-                }
-                // 2 centre
-                if((abs(temp[0].F) == abs(temp[2].F) && abs(temp[1].S) == abs(temp[2].S)) || (abs(temp[0].S) == abs(temp[2].S) && abs(temp[1].F) == abs(temp[2].F))) {
-                    if((abs(temp[0].F) == abs(temp[2].F) && abs(temp[1].S) == abs(temp[2].S))) {
-                        ans = max(ans,abs(temp[0].S-temp[2].S)*abs(temp[1].F-temp[2].F));
-                    } else {
-                        ans = max(ans,abs(temp[0].F-temp[2].F)*abs(temp[1].S-temp[2].S));
-                    }
-                }*/
             }
         }
     }
     cout << ans << "\n";
-} 
-
-int32_t main() {
-    //~ fileio 
-    fin("triangles.in"); fout("triangles.out");
-    fastio
-    int t = 1; // cin >> t; 
-    while(t--) solve();
     return 0;
 }
-
