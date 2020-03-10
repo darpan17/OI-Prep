2c9ef85fd7815db7679fa52f828e3bce3bd938ca
darpan17 <54431407+darpan17@users.noreply.github.com>
2020-03-11 02:05:53 +0530

    AC
    segtree + binary search

--- USACO/2017-2018/DEC/Gold/haybale_feast_p3.cpp
+++ USACO/2017-2018/DEC/Gold/haybale_feast_p3.cpp
@@ -68,7 +68,7 @@ signed main() {
     #endif
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    int t = 1; // cin >> t;
+    int t = 1; //  cin >> t;
     while(t--) solve();
     return 0;
 }
