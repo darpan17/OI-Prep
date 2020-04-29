7eb3d6470e6747172a991c42fb4026aea9608484
darpan17 <54431407+darpan17@users.noreply.github.com>
2020-04-29 19:50:43 +0530

    AC
    brute force + implementation

--- USACO/2016-2017/DEC/Silver/cities_and_states_p2.cpp
+++ USACO/2016-2017/DEC/Silver/cities_and_states_p2.cpp
@@ -14,36 +14,28 @@
 #define F0R(i,n) for(auto (i) = 0; (i) < (n); (i)++)
 #define FOR(i,a,b) for(auto (i) = (a); (i) <= (b); (i)++)
 #define ROF(i,a,b) for(auto (i) = (a); (i) >= (b); (i)--)
+#define pss pair<string,string>
 
 using namespace std;
 
-int h(string& x) {
-	return (x[0]*221+x[1]*122);
-} 
-
 int main() {
-	ios_base::sync_with_stdio(false); 
+	ios_base::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
 	ifstream fin("citystate.in");
 	ofstream fout("citystate.out");
 	#define cin fin
 	#define cout fout
 	int n; cin >> n;
-	multiset<pair<int,int>> s;
+	pss a[n]; F0R(i,n) cin >> a[i].F >> a[i].S;
+	map<pss,int> cnt;
 	F0R(i,n) {
-		string a,b; cin >> a >> b;
-		a = a.substr(0,2);
-		s.insert(mp(h(a),h(b)));
+		++cnt[mp(a[i].F.substr(0,2), a[i].S)];
 	}
 	int ans = 0;
-	while(!s.empty()) {
-		pii temp = *s.begin();
-		if(temp.F==temp.S) { s.erase(s.begin()); continue; } 
-		swap(temp.F, temp.S);
-		int lol = s.count(temp);
-		ans += lol*(lol+1) >> 1;
-		s.erase(temp);
-		s.erase(s.begin());
+	F0R(i,n) {
+		if(a[i].F.substr(0,2) == a[i].S) continue;
+		ans += cnt[mp(a[i].S, a[i].F.substr(0,2))];
 	}
+	ans /= 2;
 	cout << ans;
 }	
