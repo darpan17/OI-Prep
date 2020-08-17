061ee617e2e8ead0ae5260af4d717c5778f455c1
winxtron <54431407+winxtron@users.noreply.github.com>
2020-08-17 22:25:46 +0530

    AC
    Edi seen.
    dfs + offline queries (hard to come up with the idea for the soln)

--- USACO/2019-2020/DEC/Gold/milk_visits_p2.cpp
+++ USACO/2019-2020/DEC/Gold/milk_visits_p2.cpp
@@ -1,95 +1,96 @@
+/*
+SOLUTION : same as edi
+*/
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int ll;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
-
-#define mp make_pair
-#define pb push_back
+#define F0R(i,n) for(int i = 0; i < (n); i++)
+#define FOR(i,a,b) for(int i = (a); i <= (b); i++)
 #define F first
 #define S second
-#define SZ(x) (int) (x).size()
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
 
-#define FOR(i,a,b) for(int i = (a); i <= (b); i++)
-#define F0R(i,n) FOR(i,0,(n)-1)
-#define ROF(i,a,b) for(int i = (a); i >= (b); i--)
-
-const ll MOD = 1e9+7;
-const int MAXN = 2e5+5;
-const int INF = 1e9+21;
-const ll LINF = 1e18+5;
-const int xd[4] = {1,0,-1,0}, yd[4] = {0,1,0,-1}; 
-
-mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count()); 
-void IO() { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
-
+const int MAXN = 1e5+5;
 int n,m;
-vi euler;
-int a[MAXN];
-int focc[MAXN],foccidx[MAXN];
+int t[MAXN];
+pair<int,int> queryData[MAXN];
+int c[MAXN];
+vector<int> adj[MAXN];
+vector<int> ord;
+vector<pair<int,int>> store[MAXN];
+pair<int,int> tym[MAXN];
+vector<int> todo[MAXN]; // contains query indices
 bool ans[MAXN];
-vi adj[MAXN];
-set<int> ainds[MAXN];
+int timer;
 
-struct Query {
-	int l,r,t,idx;
-	Query() { l = r = t = idx = 0; }
-	Query(int _l, int _r, int _t, int _idx) { l = _l; r = _r; t = _t; idx = _idx; }
-	
-	bool operator <(const Query& other) {
-		if(focc[l] < focc[other.l]) return true;
-		else if(focc[l] == focc[other.l] && focc[r] < focc[other.r]) return true;
-		return false;
-	}
-};
-
-void dfs(int node, int p) {
-	euler.pb(node);
-	if(focc[node] == -1) focc[node] = SZ(euler)-1;
-	bool f = true;
-	for(int u : adj[node]) {
-		if(u == p) continue;
-		if(!f) euler.pb(node);
-		else f = false;
-		dfs(u,node);
-		euler.pb(-node);
-	}
+// whether u is an ancestor of v
+bool isAnc(int u, int v) {
+	return (tym[u].F <= tym[v].F && tym[u].S >= tym[v].S);
 }
 
-int main() {
-	scanf("%d%d",&n,&m);
-	F0R(i,n) scanf("%d",&a[i]);
-	F0R(i,n-1) {
-		int u,v; scanf("%d%d",&u,&v);
-		--u; --v;
-		adj[u].pb(v);
-		adj[v].pb(u);
+void dfs1(int node, int p) {
+	tym[node].F = timer++;
+	for(int u : adj[node]) {
+		if(u == p) continue;
+		dfs1(u,node);
 	}
-	vector<Query> qa(m);
-	F0R(i,m) {
-		scanf("%d%d%d",&qa[i].l,&qa[i].r,&qa[i].t);
-		--qa[i].l; --qa[i].r;
-		qa[i].idx = i;
-	}
+	tym[node].S = timer-1;
+}
+void dfs2(int node, int p) {
+	store[t[node]].push_back({node,ord.size()});
+	ord.push_back(node);
+	
+	if(todo[node].size() > 0) {
+		for(int query: todo[node]) {
+			if(store[c[query]].size() > 0) {
+				int farm = store[c[query]].back().F;
+				int dep = store[c[query]].back().S;
 
-	dfs(0,-1);
-	sort(all(qa));
-	queue<pii> qu;
-	int qptr = 0;
+				if(farm == node) ans[query] = true;
+				else {
+					assert(queryData[query].F == node || queryData[query].S == node);
+					int otherFarm = queryData[query].F+queryData[query].S-node;
 
-	int esz = SZ(euler);
-	F0R(i,esz) {
-		if(focc[euler[i]] == i) {
-			while(qptr < m && qa[qptr].l == euler[i]) {
-				ainds[a[euler[i]]].insert()
+					int farmAtReqDep = ord[dep+1]; // potential lca
+
+					// this means it lies on path of [node,lca]
+					// if farmAtReqDep is lca, that means farm is not lca, hence ans will be false
+					// but if farmAtReqDep is not lca, that means farm is lca
+					if(!isAnc(farmAtReqDep,otherFarm)) {
+						// cout << "farmAtReqDep = " << farmAtReqDep << " farm = " << farm << endl;
+						ans[query] = true;
+					}
+
+				}
 			}
 		}
 	}
+
+	for(int u : adj[node]) if(u != p) dfs2(u,node);
+	store[t[node]].pop_back();
+	ord.pop_back();
+}
+
+int main() {
+	freopen("milkvisits.in","r",stdin);
+	freopen("milkvisits.out","w",stdout);
+	scanf("%d%d",&n,&m);
+	F0R(i,n) scanf("%d",&t[i]);
+	F0R(i,n-1) {
+		int u,v; scanf("%d%d",&u,&v);
+		--u; --v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	F0R(i,m) {
+		int u,v,type; scanf("%d%d%d",&queryData[i].F,&queryData[i].S,&c[i]);
+		--queryData[i].F; --queryData[i].S;
+		todo[queryData[i].F].push_back(i);
+		todo[queryData[i].S].push_back(i);
+	}
+	dfs1(0,-1);
+	dfs2(0,-1);
+	F0R(i,m) printf("%d",(int)ans[i]);
 }
