d757d805e21c93d580e242e6933ddb38d09f30c8
winxtron <54431407+winxtron@users.noreply.github.com>
2020-09-19 09:05:55 +0530

    AC
    Soln seen
    Soln link: https://github.com/pushkarmishra/IOISolutions/blob/master/2014/Game/game-solution.pdf

--- IOI/2014_d1_p3_game.cpp
+++ IOI/2014_d1_p3_game.cpp
@@ -3,26 +3,53 @@
 using namespace std;
 
 const int MAXN = 1505;
-int el, comps;
-bool done[MAXN];
+int root[MAXN];
+int edgecnt[MAXN][MAXN];
+int n;
 
-void initialize(int n) {
-	el = (n*(n-1))/2;
-	comps = n;
+int findRoot(int node) {
+	if(root[node] == node) return node;
+	return root[node] = findRoot(root[node]);
 }
+
+bool sameComp(int u, int v) {
+	return findRoot(u) == findRoot(v);
+}
+
+void unite(int u, int v) {
+	if(u > v) swap(u,v);
+	u = findRoot(u);
+	v = findRoot(v);
+	for(int i=0;i<n;i++) {
+		if(root[i] == v) root[i] = u;
+	}
+	for(int i=0;i<n;i++) {
+		if(i == u) continue;
+		edgecnt[i][u] += edgecnt[i][v];
+		edgecnt[u][i] += edgecnt[v][i];
+	}
+}
+
+void initialize(int _n) {
+	n = _n;
+	for(int i=0;i<n;i++) root[i] = i;
+	for(int i=0;i<n;i++) {
+		for(int j=0;j<n;j++) {
+			edgecnt[i][j] = 1;
+		}
+	}
+}
+
 
 int hasEdge(int u, int v) {
-	int res;
-	if(!done[u]&!done[v]) {
-		done[u] = done[v] = true;
-		res = 1;
-	} else if(el == comps-1) {
-		res = 1;
+	if(sameComp(u,v)) return 0;
+	u = findRoot(u); v = findRoot(v);
+	if(edgecnt[u][v] == 1) {
+		unite(u,v);
+		return 1;
 	} else {
-		res = 0;
+		--edgecnt[u][v];
+		--edgecnt[v][u];
+		return 0;
 	}
-	if(res == 1) --comps;
-	--el;
-	return res;
 }
-
